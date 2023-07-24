cd3c1b38b00f048ca2935b1832f0c5e6deda0904
vlmarch <68401718+vlmarch@users.noreply.github.com>
2023-07-24 17:29:37 +0200

    Add Booth function example

---
+++ examples/booth_function_optimization.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
+from galgopy import GA, ChromosomeTemplate, Population
+from galgopy.gtypes import FloatType
+
+
+def fitness_calc(chromosome):
+    x = chromosome[0].value
+    y = chromosome[1].value
+    return (x + 2 * y - 7) ** 2 + (2 * x + y - 5) ** 2
+
+
+ct = ChromosomeTemplate([FloatType(-10, 10), FloatType(-10, 10)])
+
+population = Population.generate_random_population(
+    population_size=10, chromosome_tmplt=ct
+)
+
+ga = GA(
+    population=population,
+    fitness_func=fitness_calc,
+    fitness_mode="minimize",
+    expected_fitness=0,
+)
+ga.start()
