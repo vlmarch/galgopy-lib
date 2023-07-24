8ecb59ea51318464691e6797d63de02bbda26b23
vlmarch <68401718+vlmarch@users.noreply.github.com>
2023-07-24 17:29:00 +0200

    simple_binary.py example update

--- examples/simple_binary.py
+++ examples/simple_binary.py
@@ -5,14 +5,13 @@ sys.path.append(os.getcwd())
 
 from galgopy import GA, ChromosomeTemplate, Population
 
-
-def fitness_calc(chromosome):
-    return sum([g.value for g in chromosome])
-
-
 population = Population.generate_random_population(
     population_size=10, chromosome_tmplt=ChromosomeTemplate()
 )
 
-ga = GA(population=population, fitness_func=fitness_calc, expected_fitness=8)
+ga = GA(
+    population=population,
+    fitness_func=lambda chromosome: sum([g.value for g in chromosome]),
+    expected_fitness=8,
+)
 ga.start()
