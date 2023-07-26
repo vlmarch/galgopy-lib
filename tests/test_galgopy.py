b644549e9d0e3ddf780e658e22d864ba78f40e71
vlmarch <68401718+vlmarch@users.noreply.github.com>
2023-07-26 19:31:07 +0200

    Update tests

--- tests/test_galgopy.py
+++ tests/test_galgopy.py
@@ -6,29 +6,29 @@ sys.path.append(os.getcwd())
 import pytest
 
 import galgopy as galgopy
-import galgopy.gtypes as gtypes
+import galgopy.genetypes as genetypes
 
 validation_data = [
-    (1, gtypes.BinaryType(), True),
-    (0, gtypes.BinaryType(), True),
-    (0.5, gtypes.BinaryType(), False),
-    (5, gtypes.BinaryType(), False),
-    ("a", gtypes.BinaryType(), False),
-    (0, gtypes.IntType(), True),
-    (0.5, gtypes.IntType(), False),
-    (5, gtypes.IntType(), True),
-    ("a", gtypes.IntType(), False),
-    (0.5, gtypes.StrType(), False),
-    (5, gtypes.StrType(), False),
-    ("a", gtypes.StrType(), True),
-    ("a", gtypes.StrType("lowercase"), True),
-    ("A", gtypes.StrType("lowercase"), False),
-    ("a", gtypes.StrType("uppercase"), False),
-    ("A", gtypes.StrType("uppercase"), True),
-    ("*", gtypes.StrType(), False),
-    ("*", gtypes.CostumeType(["*", "a", 1]), True),
-    ("a", gtypes.CostumeType(["*", "a", 1]), True),
-    ("A", gtypes.CostumeType(["*", "a", 1]), False),
+    (1, genetypes.BinaryType(), True),
+    (0, genetypes.BinaryType(), True),
+    (0.5, genetypes.BinaryType(), False),
+    (5, genetypes.BinaryType(), False),
+    ("a", genetypes.BinaryType(), False),
+    (0, genetypes.IntType(), True),
+    (0.5, genetypes.IntType(), False),
+    (5, genetypes.IntType(), True),
+    ("a", genetypes.IntType(), False),
+    (0.5, genetypes.StrType(), False),
+    (5, genetypes.StrType(), False),
+    ("a", genetypes.StrType(), True),
+    ("a", genetypes.StrType("lowercase"), True),
+    ("A", genetypes.StrType("lowercase"), False),
+    ("a", genetypes.StrType("uppercase"), False),
+    ("A", genetypes.StrType("uppercase"), True),
+    ("*", genetypes.StrType(), False),
+    ("*", genetypes.CostumeType(["*", "a", 1]), True),
+    ("a", genetypes.CostumeType(["*", "a", 1]), True),
+    ("A", genetypes.CostumeType(["*", "a", 1]), False),
 ]
 
 
