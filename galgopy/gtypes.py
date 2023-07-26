5d6d7bba4b6955d6a441df3634cce716913ca9e9
vlmarch <68401718+vlmarch@users.noreply.github.com>
2023-07-26 18:47:15 +0200

    Update gtypes.py docstrings

--- galgopy/gtypes.py
+++ galgopy/gtypes.py
@@ -1,3 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Module with gene types.
+
+Module includes:
+    AbstractGeneType
+    BinaryType
+    IntType
+    FloatType
+    StrType
+    CostumeType
+
+"""
+
 import numbers
 import random
 import string
@@ -5,6 +18,8 @@ from abc import ABC, abstractmethod
 
 
 class AbstractGeneType(ABC):
+    """Abstract Gene type."""
+
     def __init__(self) -> None:
         pass
 
@@ -18,14 +33,27 @@ class AbstractGeneType(ABC):
 
     @abstractmethod
     def get_random_val(self):
-        pass
+        """Generates a random value for the gene type.
+
+        Returns:
+            Random value.
+        """
 
     @abstractmethod
-    def validate(self, n):
-        pass
+    def validate(self, val) -> bool:
+        """Validation of value. Whether it corresponds to a given type.
+
+        Args:
+            n : Value to be verified.
+
+        Returns:
+            bool: True if it corresponds to a type. False - if not.
+        """
 
 
 class BinaryType(AbstractGeneType):
+    """Implementation of binary gene type. Possible options: 0 or 1."""
+
     def __eq__(self, __o: object) -> bool:
         return isinstance(__o, type(self))
 
@@ -35,12 +63,20 @@ class BinaryType(AbstractGeneType):
     def get_random_val(self):
         return random.randint(0, 1)
 
-    def validate(self, n):
-        return n in [0, 1]
+    def validate(self, val):
+        return val in [0, 1]
 
 
 class IntType(AbstractGeneType):
+    """Implementation of integer gene type."""
+
     def __init__(self, min_val=0, max_val=9) -> None:
+        """Integer gene type.
+
+        Args:
+            min_val (int, optional): Minimum value. Defaults to 0.
+            max_val (int, optional): Maximum value. Defaults to 9.
+        """
         super().__init__()
         self._min_val = min(min_val, max_val)
         self._max_val = max(min_val, max_val)
@@ -58,12 +94,20 @@ class IntType(AbstractGeneType):
     def get_random_val(self):
         return random.randint(self._min_val, self._max_val)
 
-    def validate(self, n):
-        return n in range(self._min_val, self._max_val + 1)
+    def validate(self, val):
+        return val in range(self._min_val, self._max_val + 1)
 
 
 class FloatType(AbstractGeneType):
-    def __init__(self, min_val=0, max_val=9) -> None:
+    """Implementation of float gene type. Possible options: [min_val, max_val)."""
+
+    def __init__(self, min_val=0, max_val=1) -> None:
+        """Float gene type.
+
+        Args:
+            min_val (int, optional): Minimum value. Defaults to 0.
+            max_val (int, optional): Maximum value. Defaults to 1.
+        """
         super().__init__()
         self._min_val = min(min_val, max_val)
         self._max_val = max(min_val, max_val)
@@ -82,18 +126,29 @@ class FloatType(AbstractGeneType):
         num = random.random() * (self._max_val - self._min_val) + self._min_val
         return num
 
-    def validate(self, n):
-        return isinstance(n, numbers.Number) and (
-            self._min_val <= n < self._max_val
+    def validate(self, val):
+        return isinstance(val, numbers.Number) and (
+            self._min_val <= val < self._max_val
         )
 
 
 class StrType(AbstractGeneType):
+    """Implementation of string gene type."""
+
     def __init__(self, mode="all") -> None:
+        """_summary_
+
+        Args:
+            mode (str, optional): String type mode.
+            Modes of choice: 'all', 'lowercase', 'uppercase'. Defaults to "all".
+
+        Raises:
+            ValueError: Incorrect mode.
+        """
         super().__init__()
         if mode not in ["all", "lowercase", "uppercase"]:
             raise ValueError(
-                f"'{mode}' mode is not correct. Modes of choice: 'all', 'lowercase', 'uppercase'."
+                f"Incorrect mode: '{mode}'. Modes of choice: 'all', 'lowercase', 'uppercase'."
             )
         self._mode = mode
         if self._mode == "lowercase":
@@ -112,12 +167,19 @@ class StrType(AbstractGeneType):
     def get_random_val(self):
         return random.choice(self._data)
 
-    def validate(self, n):
-        return n in self._data
+    def validate(self, val):
+        return val in self._data
 
 
 class CostumeType(AbstractGeneType):
+    """Implementation of costume gene type."""
+
     def __init__(self, costume_data: list) -> None:
+        """Costume gene type.
+
+        Args:
+            costume_data (list): List of possible gene values.
+        """
         super().__init__()
         self._costume_data = costume_data
 
@@ -133,5 +195,5 @@ class CostumeType(AbstractGeneType):
     def get_random_val(self):
         return random.choice(self._costume_data)
 
-    def validate(self, n):
-        return n in self._costume_data
+    def validate(self, val):
+        return val in self._costume_data
