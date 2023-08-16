ee2d73fea6bc9770a0ef0faacba51c1f72d8bc34
vlmarch <68401718+vlmarch@users.noreply.github.com>
2023-08-16 19:45:29 +0200

    Update Genetic algorithm class

--- galgopy/galgopy.py
+++ galgopy/galgopy.py
@@ -1,48 +1,137 @@
-from .crossover import UniformCrossover
-from .mutation import RandomMutation
+# -*- coding: utf-8 -*-
+"""Modul with implementation of genetic algorithm.
+
+Module includes:
+    GA
+
+"""
+
+from .crossover import AbstractCrossover, UniformCrossover
+from .mutation import AbstractMutation, RandomMutation
+from .objects import Chromosome, Population
 
 
 class GA:
+    """GA - Genetic algorithm."""
+
     def __init__(
         self,
-        population,
+        initial_population: Population,
         fitness_func,
-        fitness_mode="maximize",
-        parents_count=2,
-        crossover=UniformCrossover(),
-        mutation=RandomMutation(),
-        max_generations=100,
-        expected_fitness=None,
+        fitness_mode: str = "maximize",
+        parents_count: int = 2,
+        crossover: AbstractCrossover = UniformCrossover(),
+        mutation: AbstractMutation = RandomMutation(),
+        next_population_size: int = None,
     ) -> None:
-        self._population = population
+        """GA - Genetic algorithm.
+
+        Args:
+            initial_population (Population): Initial population.
+            fitness_func (_type_): Fitness function.
+            fitness_mode (str, optional): itness mode. Options: "maximize" and
+                "minimize". Defaults to "maximize".
+            parents_count (int, optional): The number of parents for the next
+                generation. Defaults to 2.
+            crossover (AbstractCrossover, optional): Crossower for use in a
+                genetic algorithm. Defaults to UniformCrossover().
+            mutation (AbstractMutation, optional): Mutation for use in a
+                genetic algorithm. Defaults to RandomMutation().
+            next_population_size (int, optional): The number of cromosomes in
+                the new population. None - gave the same amount as in the
+                initial population. Defaults to None.
+        """
+        self._population = initial_population
         self._fitness_func = fitness_func
         self._fitness_mode = fitness_mode
         self._parents_count = parents_count
         self._crossover = crossover
         self._mutation = mutation
-        self._max_generations = max_generations
-        self._expected_fitness = expected_fitness
+        self._next_population_size = next_population_size
 
-    def start(self):
-        temp_fitnes = None
-        generation = 1
-        while not (
-            (self._expected_fitness == temp_fitnes != None)
-            or (generation == self._max_generations)
+        self._generation = 0
+        self._best_solution = None
+        self._best_fitness = None
+        self._generation_best_solution = None
+        self._generation_best_fitness = None
+        self._generation_worst_solution = None
+        self._generation_worst_fitness = None
+
+        self._fitness()
+
+    def __str__(self) -> str:
+        return f"GA( generation={self._generation} )"
+
+    def __next__(self) -> dict:
+        self._generate_next()
+        data = {
+            "Generation": self._generation,
+            "Best solution": self._best_solution,
+            "Best fitness": self._best_fitness,
+            "Generation best solution": self._generation_best_solution,
+            "Generation best fitness": self._generation_best_fitness,
+            "Generation worst solution": self._generation_worst_solution,
+            "Generation worst fitness": self._generation_worst_fitness,
+        }
+        return data
+
+    def __iter__(self):
+        return self
+
+    def _update_best_solution(self, chromosome: Chromosome):
+        if (
+            self._best_fitness is None
+            or (
+                chromosome.fitness > self._best_fitness
+                and self._fitness_mode == "maximize"
+            )
+            or (
+                chromosome.fitness < self._best_fitness
+                and self._fitness_mode == "minimize"
+            )
         ):
-            self._population.fitness(
-                self._fitness_func, mode=self._fitness_mode
-            )
-            parents = self._population.get_parents(self._parents_count)
+            self._best_solution = chromosome
+            self._best_fitness = chromosome.fitness
 
-            temp_fitnes = parents[0].fitness
-            print(
-                f"Generation {generation}: {parents[0]}. Fitness: {parents[0].fitness}."
-            )
+    def _fitness(self):
+        self._population.fitness(
+            func=self._fitness_func, mode=self._fitness_mode
+        )
 
-            population = self._crossover.generate_new_population(
-                parents, len(self._population)
-            )
-            population.apply_mutation(self._mutation)
-            self._population = population
-            generation += 1
+        self._update_best_solution(self._population[0])
+        self._generation_best_solution = self._population[0]
+        self._generation_best_fitness = self._population[0].fitness
+        self._generation_worst_solution = self._population[-1]
+        self._generation_worst_fitness = self._population[-1].fitness
+
+    def _generate_next(self):
+        parents = self._population.get_parents(
+            parents_count=self._parents_count
+        )
+
+        population = self._crossover.generate_new_population(
+            parents=parents,
+            next_population_size=len(self._population)
+            if not self._next_population_size
+            else self._next_population_size,
+        )
+
+        population.apply_mutation(mutation=self._mutation)
+        self._population = population
+        self._generation += 1
+        self._fitness()
+
+    def run(self, generations: int = 100, expected_fitness: float = None):
+        """Runs a genetic algorithm.
+
+        Args:
+            generations (int, optional): Number of generations to be generated.
+                Defaults to 100.
+            expected_fitness (float, optional): Expected fitness value.
+                None - for the generation of the total number of generations.
+                Defaults to None.
+        """
+        for _ in range(generations):
+            if expected_fitness == self._best_fitness:
+                return
+            self._generate_next()
